de1b043d379c1bab6136ad3c5425b5711f00007c
Marcin Antczak <marcin.antczak@neutrico.eu>
2016-08-16 14:56:11 +0200

    clock implemented

--- bs-config.js
+++ bs-config.js
@@ -20,33 +20,59 @@ module.exports = {
     "ui": false,
     "files": [
 	'_site',
+
+	// render scss files only, do not touch _underscored styles.
+	// scss files with names starting with _ should contain default variables only
 	{
-	    "match": ['../_styles/*.*css'],
+	    "match": ['_site/assets/styles/[a-zA-Z0-9]*.scss'],
 	    "fn": function(event, file){
-		// console.log('sass:', file);
-		var outFile = path.basename(file, '.scss');
-		outFile = '_site/' + outFile + '.css';
-		// console.log('css:', outFile);	    
-		sass.render({
-		    file: file,
-		    outFile: outFile
-		}, function(error, result){
-		    if(!error){
-			fs.writeFile(outFile, result.css, function(err){
-			    // 
-			});
-		    }
-		});
+		// console.log('event: ', event);
+		if(event == 'change' || event == 'add'){
+		    // console.log('sass:', file);
+		    var outFile, mapFile;
+
+		    outFile = path.join(path.dirname(file), path.basename(file, '.scss') + '.css');
+		    mapFile = path.join(path.dirname(file), path.basename(file, '.scss') + '.css.map');
+
+		    // when we edit scss in dev tools then write back to project
+		    fs.copy(file, path.join('../_styles/' + path.basename(file)));
+		    
+		    sass.render({
+			file: file,
+			outFile: outFile,
+			sourceMap: true
+		    }, function(error, result){
+			if(!error){
+			    fs.writeFileSync(outFile, result.css);
+			    fs.writeFileSync(mapFile, result.map);	
+			}
+		    });
+		}
 	    }
-	}, {
+	},
+	
+	{
+	    "match": ['_site/assets/scripts/*.js'],
+	    "fn": function(event, file){
+		if(event == 'change' || event == 'add'){
+		    fs.copy(file, path.join('../_scripts/' + path.basename(file)));
+		}
+	    }
+	},
+
+	// service worker files
+	{
 	    "match": ['../_scripts/*.js'],
 	    "fn": function(event, file){
 		fs.createReadStream(file).pipe(fs.createWriteStream('_site/assets/scripts/'+path.basename(file)));
 	    }
-	}, {
+	},
+
+	// markup files rendered by generator
+	{
 	    "match": ['../_includes/*', '../_layouts/*', '../*.html', '../*.md'],
 	    "fn": function(event, file){		
-		if(event === 'change') {
+		if(event == 'change' || event == 'add') {
 		    console.log('jekyll:', file);
 		    cp.spawn('jekyll', ['build', '--config' ,'config.yml,../_config.yml', '-s', '../', '-d', './_site/', '--incremental', '--quiet'], {stdio: 'inherit'}).on('close', function(code){
 			if (code > 0) {
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-var bs = require('browser-sync');
+'use strict';
+var bs = require('browser-sync').create();
+var fs = require('fs-extra');
 var bsconf = require('./bs-config.js');
 
-process.title = process.argv[2];
-console.log("server " + process.title);
+const mkdirp = require('mkdirp-promise');
+const exec = require('child-process-promise').exec;
 
-bs.init(bsconf, function(){ console.log('bs')});
+let setScripts  = mkdirp('./_site/assets/scripts')
+    .then((result) => console.log('Directory created:\n', result));
+
+let setStyles  = mkdirp('./_site/assets/styles')
+    .then((result) => console.log('Directory created:\n', result));
+
+let setMarkup = exec('jekyll build -s ../ -d ./_site')
+    .then((result) => console.log('Markup generated:\n', result.stdout));
+
+Promise.all([setScripts, setStyles, setMarkup]).then(value => {
+    bs.init(bsconf, () => {
+	fs.copy('../_scripts', './_site/assets/scripts');
+	fs.copy('../_styles', './_site/assets/styles');
+    });
+}, reason => {
+    console.log(reason);
+});
